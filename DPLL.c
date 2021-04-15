b9b50b77873bfc13028fa307a7773035358c2851
yaolao0107 <79920509+yaolao0107@users.noreply.github.com>
2021-04-15 13:06:57 +0800

    Update DPLL.c

--- DPLL.c
+++ DPLL.c
@@ -1,10 +1,10 @@
 #include "head.h"
 /*输入子句集S，按照MOM策略选择子句集中一个文字，并且输出文字的data值*/
-ElemType SelectLetter(SNode *ps)
+ElemType SelectLetter(SNode* ps)
 {
     int min = ps->bignode.nextC->letternum;
-    CNode *pc = ps->bignode.nextC;
-    CNode *pct = ps->bignode.nextC; //t=traverse
+    CNode* pc = ps->bignode.nextC;
+    CNode* pct = ps->bignode.nextC; //t=traverse
     while (pct)
     {
         if (min <= pct->letternum)
@@ -19,15 +19,15 @@ ElemType SelectLetter(SNode *ps)
     return pc->headnode.nextL->data; //选最短子句的一个文字
 }
 
-ElemType SelectLetter_og(SNode *ps)
+ElemType SelectLetter_og(SNode* ps)
 {
-    ElemType var = rand() % var_num+1;
+    ElemType var = rand() % var_num + 1;
     return var;
 }
 //选择一个单子句,选中后返回指向单子句的指针pct，否则返回NULL
-CNode *SelectUnitC(SNode *ps)
+CNode* SelectUnitC(SNode* ps)
 {
-    CNode *pc = ps->bignode.nextC;
+    CNode* pc = ps->bignode.nextC;
     while (pc)
     {
         if (pc->letternum == 1)
@@ -40,20 +40,20 @@ CNode *SelectUnitC(SNode *ps)
 
 //删除含文字L的所有子句
 //ps为子句集，data为要删除的文字
-status Del_CwithL(SNode *ps, ElemType data)
+status Del_CwithL(SNode* ps, ElemType data)
 {
-    CNode *pc = &ps->bignode, *qc = ps->bignode.nextC; //pc指向子句集的头节点，qc指向首子句
-    LNode *pl = qc->headnode.nextL;                    //pl指向qc指向子句的首文字
+    CNode* pc = &ps->bignode, * qc = ps->bignode.nextC; //pc指向子句集的头节点，qc指向首子句
+    LNode* pl = qc->headnode.nextL;                    //pl指向qc指向子句的首文字
     int count = 0;
-    int error=0;                                     //测试bug部分计数器
+    int error = 0;                                     //测试bug部分计数器
     while (qc)
     {
         int flag = 0; //flag为是否删除当前子句qc的标志,每次遍历置0
         pl = qc->headnode.nextL;
         while (pl)
         {
-            if(pl->data==10141)
-            error=1;
+            if (pl->data == 10141)
+                error = 1;
             if (pl->data == data)
             {
                 flag = 1;
@@ -78,11 +78,11 @@ status Del_CwithL(SNode *ps, ElemType data)
 }
 
 //删除所有子句中的文字L
-status Del_LinC(SNode *ps, ElemType data)
+status Del_LinC(SNode* ps, ElemType data)
 {
-    CNode *pc = ps->bignode.nextC;
-    LNode *pl;
-    LNode *ql;
+    CNode* pc = ps->bignode.nextC;
+    LNode* pl;
+    LNode* ql;
     while (pc)
     {
         pl = &pc->headnode;
@@ -105,9 +105,9 @@ status Del_LinC(SNode *ps, ElemType data)
     return TRUE;
 }
 //子句集S中含有空子句，返回1，否则返回0
-status IfEmptyClause(SNode *ps)
+status IfEmptyClause(SNode* ps)
 {
-    CNode *pc = ps->bignode.nextC;
+    CNode* pc = ps->bignode.nextC;
     while (pc)
     {
         if (!pc->letternum)
@@ -119,7 +119,7 @@ status IfEmptyClause(SNode *ps)
 }
 //给出子句集S，若可满足，输出TRUE,否则输出FALSE
 //var非0时激活分支策略,flag为var的真值情况
-status DPLL(SNode *ps, int var, int flag, int truthtable[])
+status DPLL(SNode* ps, int var, int flag, int truthtable[])
 {
     //分治策略,var非0激活分治策略
     if (var && flag)
@@ -132,7 +132,7 @@ status DPLL(SNode *ps, int var, int flag, int truthtable[])
         Del_CwithL(ps, -var);
         Del_LinC(ps, var);
     }
-    CNode *pct = NULL;
+    CNode* pct = NULL;
     ElemType data = 0; //保存单子句中的文字
     //单子句策略
     while (pct = SelectUnitC(ps))
@@ -147,7 +147,7 @@ status DPLL(SNode *ps, int var, int flag, int truthtable[])
             return FALSE;
     }
     ElemType v = SelectLetter(ps); //文字取真
-    SNode *ps_former = (SNode *)malloc(sizeof(SNode));
+    SNode* ps_former = (SNode*)malloc(sizeof(SNode));
     CopyList(ps, ps_former);
     if (DPLL(ps, v, 0, truthtable))
     {
@@ -163,7 +163,7 @@ status DPLL(SNode *ps, int var, int flag, int truthtable[])
         return FALSE;
 }
 
-status DPLL_og(SNode *ps, int var, int flag, int truthtable[])
+status DPLL_og(SNode* ps, int var, int flag, int truthtable[])
 {
     //分治策略,var非0激活分治策略
     if (var && flag)
@@ -176,7 +176,7 @@ status DPLL_og(SNode *ps, int var, int flag, int truthtable[])
         Del_CwithL(ps, -var);
         Del_LinC(ps, var);
     }
-    CNode *pct = NULL;
+    CNode* pct = NULL;
     ElemType data = 0; //保存单子句中的文字
     //单子句策略
     while (pct = SelectUnitC(ps))
@@ -191,7 +191,7 @@ status DPLL_og(SNode *ps, int var, int flag, int truthtable[])
             return FALSE;
     }
     ElemType v = SelectLetter_og(ps); //文字取真
-    SNode *ps_former = (SNode *)malloc(sizeof(SNode));
+    SNode* ps_former = (SNode*)malloc(sizeof(SNode));
     CopyList(ps, ps_former);
     if (DPLL_og(ps, v, 0, truthtable))
     {
@@ -207,17 +207,17 @@ status DPLL_og(SNode *ps, int var, int flag, int truthtable[])
         return FALSE;
 }
 //将ps1所指的子句集复制给ps2
-status CopyList(SNode *ps1, SNode *ps2)
+status CopyList(SNode* ps1, SNode* ps2)
 {
-    CNode *pc1 = NULL, *pc2 = NULL;
-    LNode *pl1 = NULL, *pl2 = NULL;
+    CNode* pc1 = NULL, * pc2 = NULL;
+    LNode* pl1 = NULL, * pl2 = NULL;
     ps2->clausenum = ps1->clausenum;
     pc1 = &ps1->bignode;
     pc2 = &ps2->bignode;
     while (pc1->nextC)
     {
         pc1 = pc1->nextC;
-        pc2->nextC = (CNode *)malloc(sizeof(CNode));
+        pc2->nextC = (CNode*)malloc(sizeof(CNode));
         pc2 = pc2->nextC;
         pc2->nextC = NULL;
         pc2->letternum = pc1->letternum;
@@ -227,7 +227,7 @@ status CopyList(SNode *ps1, SNode *ps2)
         while (pl1->nextL)
         {
             pl1 = pl1->nextL;
-            pl2->nextL = (LNode *)malloc(sizeof(LNode));
+            pl2->nextL = (LNode*)malloc(sizeof(LNode));
             pl2 = pl2->nextL;
             pl2->data = pl1->data;
             pl2->nextL = NULL;
